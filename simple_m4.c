e261e3ef61eb737155bc042442c7ab8ba9c69097
Brad Grantham <grantham@plunk.org>
2015-12-05 18:33:54 -0800

    volatile, so it still updates under optimization

--- simple_m4.c
+++ simple_m4.c
@@ -1,6 +1,6 @@
-main()
+int main(int argc, char **argv)
 {
-    unsigned char *blarg = (unsigned char*)0x0100000;
+    volatile unsigned char *blarg = (unsigned char*)0x0100000;
 
     for(;;)
         *blarg = *blarg + 1;
