f6f9718ef15504348a06ec2fe413c20ebb7ce675
Brad Grantham <grantham@plunk.org>
2015-12-06 14:04:21 -0800

    working LED flasher on MiniM4

--- Makefile
+++ Makefile
@@ -1,19 +1,37 @@
 OPT             =       -O2
 
-TOOLROOT        =       ~/Downloads/gcc-arm-none-eabi-4_9-2015q3/
+TOOLROOT        =       $(HOME)/trees/gcc-arm-none-eabi-4_9-2015q3
+STM32FCUBEROOT  =       $(HOME)/trees/STM32Cube_FW_F4_V1.9.0
 GCC_LIB         =       $(TOOLROOT)/lib/gcc/arm-none-eabi/4.9.3
 
-CORTEX_M4_HWFP_CC_FLAGS = -mthumb -mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16 -mlittle-endian 
+CORTEX_M4_HWFP_CC_FLAGS = -mthumb -mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16 -mlittle-endian
+TARGET = STM32F415xx
+STM32F4XX_CMSIS_INC_PATH        = $(STM32FCUBEROOT)/Drivers/CMSIS/Include/
 CORTEX_M4_HWFP_LIB_PATH = $(GCC_LIB)/armv7e-m/fpu
+
+OBJECTS = \
+        simple_m4.o \
+        startup_stm32f417xx.o \
+        system_stm32f4xx.o \
+        stm32f4xx_it.o \
+        stm32f4xx_hal_gpio.o \
+        stm32f4xx_hal_cortex.o \
+        stm32f4xx_hal_rcc.o \
+        stm32f4xx_hal.o \
+        $(NULL)
+
 
 simple_m4.hex: simple_m4.elf
 	$(TOOLROOT)/bin/arm-none-eabi-objcopy -O ihex $< $@
 
-simple_m4.elf: simple_m4.o
+simple_m4.elf: $(OBJECTS)
 	$(TOOLROOT)/bin/arm-none-eabi-gcc $(OPT) $(CORTEX_M4_HWFP_CC_FLAGS) -DSTM32F415xx -L$(CORTEX_M4_HWFP_LIB_PATH) -TSTM32F415RG_FLASH.ld -lm -specs=nosys.specs -Wl,--gc-sections $^ -o $@
 
-simple_m4.o: simple_m4.c
+startup_stm32f417xx.o: startup_stm32f417xx.s
 	$(TOOLROOT)/bin/arm-none-eabi-gcc -Wall $(OPT) $(CORTEX_M4_HWFP_CC_FLAGS)  $^ -c -o $@
+
+%.o: %.c
+	$(TOOLROOT)/bin/arm-none-eabi-gcc -std=c99 -Wall $(OPT) -I$(STM32F4XX_CMSIS_INC_PATH) -D$(TARGET) $(CORTEX_M4_HWFP_CC_FLAGS)  $^ -c -o $@
 
 clean:
 	rm simple_m4.hex *.o
--- simple_m4.c
+++ simple_m4.c
@@ -1,7 +1,134 @@
+#include "stm32f4xx_hal.h"
+
+static GPIO_InitTypeDef  GPIO_InitStruct;
+
+// From Projects/STM32F411RE-Nucleo/Examples/GPIO/GPIO_IOToggle/Src/main.c
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @param  None
+  * @retval None
+  */
+static void Error_Handler(void)
+{
+  while(1)
+  {
+     HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_0);
+  }
+}
+
+// From Projects/STM32F411RE-Nucleo/Examples/GPIO/GPIO_IOToggle/Src/main.c
+/**
+  * @brief  System Clock Configuration
+  *         The system Clock is configured as follow : 
+  *            System Clock source            = PLL (HSI)
+  *            SYSCLK(Hz)                     = 100000000
+  *            HCLK(Hz)                       = 100000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 2
+  *            APB2 Prescaler                 = 1
+  *            HSI Frequency(Hz)              = 16000000
+  *            PLL_M                          = 16
+  *            PLL_N                          = 400
+  *            PLL_P                          = 4
+  *            PLL_Q                          = 7
+  *            VDD(V)                         = 3.3
+  *            Main regulator output voltage  = Scale2 mode
+  *            Flash Latency(WS)              = 3
+  * @param  None
+  * @retval None
+  */
+static void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+
+  /* Enable Power Control clock */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  
+  /* The voltage scaling allows optimizing the power consumption when the device is 
+     clocked below the maximum system frequency, to update the voltage scaling value 
+     regarding system frequency refer to product datasheet.  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+  
+  /* Enable HSI Oscillator and activate PLL with HSI as source */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = 0x10;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 16;
+  RCC_OscInitStruct.PLL.PLLN = 400;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  if(HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 
+     clocks dividers */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;  
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;  
+  if(HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+
 int main(int argc, char **argv)
 {
-    volatile unsigned char *blarg = (unsigned char*)0x0100000;
+    volatile unsigned char blarg;
 
-    for(;;)
-        *blarg = *blarg + 1;
+    // From Projects/STM32F411RE-Nucleo/Examples/GPIO/GPIO_IOToggle/Src/main.c
+    /* STM32F4xx HAL library initialization:
+       - Configure the Flash prefetch, instruction and Data caches
+       - Configure the Systick to generate an interrupt each 1 msec
+       - Set NVIC Group Priority to 4
+       - Global MSP (MCU Support Package) initialization
+     */
+    HAL_Init();
+
+    /* Configure the system clock to 100 MHz */
+    SystemClock_Config();
+
+    /*##-1- Enable GPIOA Clock (to be able to program the configuration registers) */
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    // end of code from Projects/STM32F411RE-Nucleo/Examples/GPIO/GPIO_IOToggle/Src/main.c
+
+    GPIO_InitStruct.Pin = GPIO_PIN_0 | GPIO_PIN_1;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct); 
+
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+
+    GPIO_InitStruct.Pin = GPIO_PIN_13 | GPIO_PIN_12;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct); 
+
+    int pin = 1;
+    while(1) {
+        HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_0);
+    
+        /* delay */
+        if(0) {
+            for(int i = 0; i < 2550000; i++) {
+                blarg = blarg + 1;
+            }
+        } else
+            HAL_Delay(500);
+
+        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_12);
+
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, pin);
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, pin);
+        pin = pin ? 0 : 1;
+    }
 }
